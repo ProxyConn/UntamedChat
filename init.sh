43bf06acca76471db78940e505fee9c0b67dea42
portalBlock <portalBlock@users.noreply.github.com>
2015-02-07 16:41:01 -0600

    Attempt to make auto deploys work.

---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Downloading BAT lib..."
+mkdir lib && wget http://www.spigotmc.org/resources/bungee-admin-tools.444/download?version=9384 -O lib/BAT.jar
+echo "BAT lib downloaded!"

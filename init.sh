f617eaf92ef24adf29be48d99150fe71c0ddc59a
portalBlock <portalBlock@users.noreply.github.com>
2015-02-07 16:45:09 -0600

    Permission denied errors, this might fix them.

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Downloading BAT lib..."
-mkdir lib && wget http://www.spigotmc.org/resources/bungee-admin-tools.444/download?version=9384 -O lib/BAT.jar
+sudo mkdir lib && sudo wget http://www.spigotmc.org/resources/bungee-admin-tools.444/download?version=9384 -O lib/BAT.jar
 echo "BAT lib downloaded!"
